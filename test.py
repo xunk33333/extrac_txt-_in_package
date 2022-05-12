7b91e491ca571bf4b8cc49ad6a67c0a4a248e4c2
XunKe <139978370@qq.com>
2022-05-12 11:27:24 +0800

    解决脚宽度问题

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import re
 import pdfplumber
 import pandas as pd
 
-
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
 
@@ -57,7 +55,7 @@ def test_one(file_path):
     name, information = dataset("output_csv/" + pdf.metadata['Title'] + ".csv")
 
     X = StandardScaler().fit_transform(information)
-    db = DBSCAN(eps=0.3,min_samples=3).fit(X)
+    db = DBSCAN(eps=0.3, min_samples=3).fit(X)
     # print(name)
     # print(db.labels_)
     # print(db.core_sample_indices_)
@@ -66,128 +64,138 @@ def test_one(file_path):
     # print(date)
     # print(db.labels_)
     date['label'] = db.labels_
-    date.to_csv("output_csv/" + pdf.metadata['Title'] + ".csv",index=False)
+    date.to_csv("output_csv/" + pdf.metadata['Title'] + ".csv", index=False)
     # print(pd.read_csv("output_csv/" + pdf.metadata['Title'] + ".csv"))
 
     # 读取数据文件
     df = pd.read_csv("output_csv/" + pdf.metadata['Title'] + ".csv")
-    # print([100<each<140 for each in df['x0']])
+    # print(df['text'][0]>0)
 
     # 创建结果表
     result = pd.DataFrame()
 
+    # 过滤掉最上面的小数
+    position = [
+        [each / page.height < 1 / 8.3 for each in df['doctop']],
+        [each == -1 for each in df['label']]
+    ]
+    booll = [all(e) for e in zip(*position)]
+    # result['厚度'] = df[booll]['text'].to_numpy()
+    df.drop(index=df[booll]['text'].index, inplace=True)
+
     # 厚度
-    position = [[each/page.width <3/11.5 for each in df['x0']],
-            [1/2<each/page.height < 7.1/8.3 for each in df['doctop']],
-            [each > -1 for each in df['label']]
-            ]
+    position = [[each / page.width < 2.6 / 11.5 for each in df['x0']],
+                [1 / 2 < each / page.height < 7.1 / 8.3 for each in df['doctop']],
+                [each > -1 for each in df['label']]
+                ]
     booll = [all(e) for e in zip(*position)]
     result['厚度'] = df[booll]['text'].to_numpy()
-    df.drop(index=df[booll]['text'].index,inplace=True)
+    df.drop(index=df[booll]['text'].index, inplace=True)
 
     # 外边长
-    position = [[each/page.width <4.5/11.5 for each in df['x0']],
-            [each/page.height <3.2/8.6 for each in df['doctop']]
-            ]
+    position = [[each / page.width < 4.5 / 11.5 for each in df['x0']],
+                [each / page.height < 3.2 / 8.6 for each in df['doctop']]
+                ]
     booll = [all(e) for e in zip(*position)]
-    result = pd.concat([result,pd.DataFrame({'外边长':df[booll]['text'].to_numpy()})],axis=1)
-    df.drop(index=df[booll]['text'].index,inplace=True)
+    result = pd.concat([result, pd.DataFrame({'外边长': df[booll]['text'].to_numpy()})], axis=1)
+    df.drop(index=df[booll]['text'].index, inplace=True)
 
     # EXPOSED_PAD宽度
-    position = [[each/page.width >8/11.5 for each in df['x0']],
-            [2.5/9.2< each/page.height <7/9.2 for each in df['doctop']],
+    position = [[each / page.width > 8 / 11.5 for each in df['x0']],
+                [2.5 / 9.2 < each / page.height < 7 / 9.2 for each in df['doctop']],
                 [each > -1 for each in df['label']]
-            ]
+                ]
     booll = [all(e) for e in zip(*position)]
-    result = pd.concat([result,pd.DataFrame({'EXPOSED_PAD宽度':df[booll]['text'].to_numpy()})],axis=1)
-    df.drop(index=df[booll]['text'].index,inplace=True)
+    result = pd.concat([result, pd.DataFrame({'EXPOSED_PAD宽度': df[booll]['text'].to_numpy()})], axis=1)
+    df.drop(index=df[booll]['text'].index, inplace=True)
 
     # 脚长度
-    position = [[4/12.5< each/page.width <8/12 for each in df['x0']],
-            [1/2< each/page.height <7/8.7 for each in df['doctop']],
+    position = [[4 / 12.5 < each / page.width < 8 / 12 for each in df['x0']],
+                [1 / 2 < each / page.height < 7 / 8.7 for each in df['doctop']],
                 [each > -1 for each in df['label']]
-            ]
+                ]
     booll = [all(e) for e in zip(*position)]
-    result = pd.concat([result,pd.DataFrame({'脚长度':df[booll]['text'].to_numpy()})],axis=1)
-    df.drop(index=df[booll]['text'].index,inplace=True)
+    result = pd.concat([result, pd.DataFrame({'脚长度': df[booll]['text'].to_numpy()})], axis=1)
+    df.drop(index=df[booll]['text'].index, inplace=True)
 
-    # 脚宽度
-    position = [[2/11.4< each/page.width <5.5/11.4 for each in df['x0']],
-            [6/8.4< each/page.height  for each in df['doctop']],
+    # 脚宽度1
+    position = [[2 / 11.4 < each / page.width < 5.5 / 11.4 for each in df['x0']],
+                [6 / 8.4 < each / page.height for each in df['doctop']],
                 [each > -1 for each in df['label']]
-            ]
-    booll1 = [all(e) for e in zip(*position)]
-
-    position = [[4.5/10.7< each/page.width <9/12 for each in df['x0']],
-            [1.5/8.8< each/page.height <4/8.7 for each in df['doctop']],
-                [each > -1 for each in df['label']]
-            ]
-    booll2 = [all(e) for e in zip(*position)]
-
-    position = [booll1,
-                booll2
-            ]
-    booll = [any(e) for e in zip(*position)]
-    result = pd.concat([result,pd.DataFrame({'脚宽度':df[booll]['text'].to_numpy()})],axis=1)
-    df.drop(index=df[booll]['text'].index,inplace=True)
+                ]
+    booll = [all(e) for e in zip(*position)]
+    result = pd.concat([result, pd.DataFrame({'脚宽度': df[booll]['text'].to_numpy()})], axis=1)
+    df.drop(index=df[booll]['text'].index, inplace=True)
     # print(df)
 
-    # EXPOSED_PAD长度
-    position = [[6.5/11.4< each/page.width <9.5/11.4 for each in df['x0']],
-            [1/8< each/page.height <4/8.7 for each in df['doctop']],
+    # # EXPOSED_PAD长度
+    # position = [[6.5/11.4< each/page.width <9.5/11.4 for each in df['x0']],
+    #         [1/8< each/page.height <4/8.7 for each in df['doctop']],
+    #             [each > -1 for each in df['label']]
+    #         ]
+    # booll = [all(e) for e in zip(*position)]
+    # result = pd.concat([result,pd.DataFrame({'EXPOSED_PAD长度':df[booll]['text'].to_numpy()})],axis=1)
+    # df.drop(index=df[booll]['text'].index,inplace=True)
+
+    # 脚宽度2
+    position = [[4.5 / 10.7 < each / page.width < 9 / 12 for each in df['x0']],
+                [1.5 / 8.8 < each / page.height < 4 / 8.7 for each in df['doctop']],
                 [each > -1 for each in df['label']]
-            ]
+                ]
     booll = [all(e) for e in zip(*position)]
-    result = pd.concat([result,pd.DataFrame({'EXPOSED_PAD长度':df[booll]['text'].to_numpy()})],axis=1)
-    df.drop(index=df[booll]['text'].index,inplace=True)
+    # print(df[booll]['text'].head(1).to_numpy())
+    if float(df[booll]['text'].head(1).to_numpy()) > 1:
+        result = pd.concat([result, pd.DataFrame({'EXPOSED_PAD长度': df[booll]['text'].to_numpy()})], axis=1)
+    else:
+        result = pd.concat([result, pd.DataFrame({'脚宽度': df[booll]['text'].to_numpy()})], axis=1)
+
+    df.drop(index=df[booll]['text'].index, inplace=True)
 
     if df.empty:
         pass
     else:
-        #脚间距
+        # 脚间距
         position = [
-            [ each/page.height <4.5/8.4 for each in df['doctop']],
-                [each == -1 for each in df['label']]
-            ]
+            [each / page.height < 4.5 / 8.4 for each in df['doctop']],
+            [each == -1 for each in df['label']]
+        ]
         booll = [all(e) for e in zip(*position)]
-        result = pd.concat([result,pd.DataFrame({'脚间距':df[booll]['text'].to_numpy()})],axis=1)
-        df.drop(index=df[booll]['text'].index,inplace=True)
+        result = pd.concat([result, pd.DataFrame({'脚间距': df[booll]['text'].to_numpy()})], axis=1)
+        df.drop(index=df[booll]['text'].index, inplace=True)
 
     if df.empty:
         pass
     else:
-         #拐角限制
-        position = [[8/12< each/page.width for each in df['x0']],
-            [ 4.5/8.4< each/page.height  for each in df['doctop']],
-                [each == -1 for each in df['label']]
-            ]
+        # 拐角限制
+        position = [[8 / 12 < each / page.width for each in df['x0']],
+                    [4.5 / 8.4 < each / page.height for each in df['doctop']],
+                    [each == -1 for each in df['label']]
+                    ]
         booll = [all(e) for e in zip(*position)]
-        result = pd.concat([result,pd.DataFrame({'拐角限制':df[booll]['text'].to_numpy()})],axis=1)
-        df.drop(index=df[booll]['text'].index,inplace=True)
+        result = pd.concat([result, pd.DataFrame({'拐角限制': df[booll]['text'].to_numpy()})], axis=1)
+        df.drop(index=df[booll]['text'].index, inplace=True)
 
     if df.empty:
         pass
-    else:# 剩余side view参数
+    else:  # 剩余side view参数
         df = df.sort_values('doctop')
         position = [
-            [5.5/8.7< each/page.height for each in df['doctop']],
-                [each == -1 for each in df['label']]
-            ]
+            [5.5 / 8.7 < each / page.height for each in df['doctop']],
+            [each == -1 for each in df['label']]
+        ]
         booll = [all(e) for e in zip(*position)]
         side_view = df[booll]['text']
-        df.drop(index=df[booll]['text'].index,inplace=True)
-        result = pd.concat([result,pd.DataFrame({'芯片底到pcb最大距离':side_view[0:1].to_numpy()})],axis=1)
-        side_view.drop(index=side_view[0:1].index,inplace=True)
-        result = pd.concat([result,pd.DataFrame({'芯片底到pcb标准距离':side_view[0:1].to_numpy()})],axis=1)
-        side_view.drop(index=side_view[1:2].index,inplace=True)
-        result = pd.concat([result,pd.DataFrame({'针脚到pcb距离':side_view.tail(1).to_numpy()})],axis=1)
-        side_view.drop(index=side_view.tail(1).index,inplace=True)
-        result = pd.concat([result,pd.DataFrame({'COPLANARITY':side_view.to_numpy()})],axis=1)
-        result = pd.concat([result,pd.DataFrame({'未分类':df['text'].to_numpy()})],axis=1)
+        df.drop(index=df[booll]['text'].index, inplace=True)
+        result = pd.concat([result, pd.DataFrame({'芯片底到pcb最大距离': side_view[0:1].to_numpy()})], axis=1)
+        side_view.drop(index=side_view[0:1].index, inplace=True)
+        result = pd.concat([result, pd.DataFrame({'芯片底到pcb标准距离': side_view[0:1].to_numpy()})], axis=1)
+        side_view.drop(index=side_view[0:1].index, inplace=True)
+        result = pd.concat([result, pd.DataFrame({'针脚到pcb距离': side_view.tail(1).to_numpy()})], axis=1)
+        side_view.drop(index=side_view.tail(1).index, inplace=True)
+        result = pd.concat([result, pd.DataFrame({'COPLANARITY': side_view.to_numpy()})], axis=1)
+        result = pd.concat([result, pd.DataFrame({'未分类': df['text'].to_numpy()})], axis=1)
 
-
-
-    result.to_csv("result_csv/" + pdf.metadata['Title'] + ".csv",index=False,encoding='utf-8-sig')
+    result.to_csv("result_csv/" + pdf.metadata['Title'] + ".csv", index=False, encoding='utf-8-sig')
 
 
 if __name__ == '__main__':
